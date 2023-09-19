fc756ecdc08efc7724f9d6b09d4b8ff16cc1dde4
Adrien Tolno <tus29878@temple.edu>
2023-09-19 15:26:14 -0400

    Popular the array create the item from the array in spinner change the textView size

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -13,15 +13,36 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.spinner)
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
+
+
+
         /* Step 1: Populate this array */
         //val numberArray = Array Of Numbers
+        val numberArray = Array(5) {
+            10 * (it + 1)
+        }
 
         /* Step 2: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
 
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,
+        numberArray);
+
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        spinner.onItemClickListener = object: AdapterView.OnItemClickListener{
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+               p0?.run {
+                   val sizefont = numberArray[p2]?.toFloat()
+                   if (sizefont != null)
+                       displayTextView.textSize = sizefont
+
+               }
+
+            }
+
+        }
 
     }
 }
